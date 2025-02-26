b9d23bb5dda7ef658d3a5b43edb0bd1a4d799241
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2025-02-26 19:31:14 +0200

    Add authentication, media upload, and error handling features

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,12 @@
+import MediaForm from '@/components/MediaForm';
+
+const Page = () => {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Upload</h1>
+      <MediaForm />
+    </main>
+  );
+};
+
+export default Page;
